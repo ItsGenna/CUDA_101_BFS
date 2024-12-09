7dcb138d23bf35d9c43b3201d23d57a3cad54cf4
matte <matteogennari9@gmail.com>
2024-12-09 14:09:06 +0100

    Merged remote with local project

---
+++ cmake-build-debug-wsl/CMakeFiles/BFS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BFS"
+  "BFS.pdb"
+  "CMakeFiles/BFS.dir/cmake_device_link.o"
+  "CMakeFiles/BFS.dir/main.cu.o"
+  "CMakeFiles/BFS.dir/main.cu.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/BFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
